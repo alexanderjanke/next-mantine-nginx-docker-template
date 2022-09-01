1d2e1d13848e5da9ae054bc67973a61ce5698e8e
Alexander Janke <alexander.janke.92@gmail.com>
2022-09-01 16:07:48 +0200

    add placeholder component

---
+++ components/title.tsx
@@ -0,0 +1,7 @@
+/**
+ * Title component
+ * @returns JSX.Element
+ */
+export default function Title(): JSX.Element {
+  return <h1>Title!</h1>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@mantine/core";
 
+import Title from "../components/title";
+
 /**
  * Index page
  * @returns JSX Element for index page
  */
 export default function Home(): JSX.Element {
-  return <Button>Content</Button>;
+  return (
+    <>
+      <Button>Content</Button>
+      <Title></Title>
+    </>
+  );
 }
